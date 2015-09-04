fe0b790bf5a0f7340b3713297faa686ade6e6cfe
Rahul Agarwal <reams.al@gmail.com>
2015-09-03 21:00:40 -0700

    adding measures of dispersion and binning

--- plantSpeciesAnalyser.py
+++ plantSpeciesAnalyser.py
@@ -22,11 +22,51 @@ iris = load_iris()
 X = iris.data 
 Y = iris.target
 
+
+
 print iris.DESCR
 
 iris_data = DataFrame(X,columns=['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width'])
 
 iris_target = DataFrame(Y,columns=['Species'])
+
+# Central tendency and dispersion calculation
+print 'Mean'
+print np.mean(iris_data['Sepal Length'])
+#print iris_data['Sepal Length'].mean()
+
+print 'Median'
+print np.median(iris_data['Sepal Length'])
+#print iris_data['Sepal Length'].median()
+
+print 'Mode'
+#print np.mode(iris_data['Sepal Length'])
+print iris_data['Sepal Length'].mode()
+
+print 'Quartiles'
+print '1st Quartile'
+print iris_data['Sepal Length'].quantile(0.25)
+print '3rd Quartile'
+print iris_data['Sepal Length'].quantile(0.75)
+print '4th Quartile'
+print iris_data['Sepal Length'].quantile(1.0)
+
+#attribute petal length
+#applyting binning for removing noisy data
+
+
+# this is equal width binning
+pl_data = iris_data['Petal Length']
+
+#plt.hist(data,bins=np.range(min(data),max(data) + binwidth,binwidth))
+plt.hist(pl_data,bins=np.arange(1.0,6.9 + 0.59,0.59))
+plt.show()
+
+#plt.hist(pl_data, bins=[0, 10, 20, 30, 40, 50, 100])
+
+
+
+
 
 print iris_data
 print iris_target
@@ -38,6 +78,7 @@ print iris_target.tail()
 
 iris = pd.concat([iris_data,iris_target],axis=1)
 print iris 
+
 
 sns.pairplot(iris,hue='Species',size=2)
 sns.plt.show()
